187291d6ad6f24602eb967ebe6a842256f5f0efa
5663015 <398155058@qq.com>
2020-04-29 10:20:36 +0800

    add my models

--- cnn/train.py
+++ cnn/train.py
@@ -39,14 +39,15 @@ parser.add_argument('--save', type=str, default='EXP', help='experiment name')
 parser.add_argument('--seed', type=int, default=0, help='random seed')
 parser.add_argument('--arch', type=str, default='DARTS', help='which architecture to use')
 parser.add_argument('--grad_clip', type=float, default=5, help='gradient clipping')
+parser.add_argument('--exp_name', type=str, default='full_train', help='experiment name')
 args = parser.parse_args()
 
-args.save = 'log/eval-{}-{}-{}'.format(args.save, time.strftime("%Y%m%d-%H%M%S"), str(uuid.uuid4()))
+args.save = '{}-{}'.format(args.exp_name, time.strftime("%Y%m%d"))
 utils.create_exp_dir(args.save, scripts_to_save=glob.glob('*.py'))
 
 log_format = '%(asctime)s %(message)s'
 logging.basicConfig(stream=sys.stdout, level=logging.INFO,
-    format=log_format, datefmt='%m/%d %I:%M:%S %p')
+                    format=log_format, datefmt='%m/%d %I:%M:%S %p')
 fh = logging.FileHandler(os.path.join(args.save, 'log.txt'))
 fh.setFormatter(logging.Formatter(log_format))
 logging.getLogger().addHandler(fh)
@@ -55,122 +56,123 @@ CIFAR_CLASSES = 10
 
 
 def main():
-  if not torch.cuda.is_available():
-    logging.info('no gpu device available')
-    sys.exit(1)
-
-  np.random.seed(args.seed)
-  torch.cuda.set_device(args.gpu)
-  cudnn.benchmark = True
-  torch.manual_seed(args.seed)
-  cudnn.enabled=True
-  torch.cuda.manual_seed(args.seed)
-  logging.info('gpu device = %d' % args.gpu)
-  logging.info("args = %s", args)
-
-  genotype = eval("genotypes.%s" % args.arch)
-  model = Network(args.init_channels, CIFAR_CLASSES, args.layers, args.auxiliary, genotype)
-  model = model.cuda()
-
-  logging.info("param size = %fMB", utils.count_parameters_in_MB(model))
-
-  criterion = nn.CrossEntropyLoss()
-  criterion = criterion.cuda()
-  optimizer = torch.optim.SGD(
-      model.parameters(),
-      args.learning_rate,
-      momentum=args.momentum,
-      weight_decay=args.weight_decay
-      )
-
-  train_transform, valid_transform = utils._data_transforms_cifar10(args)
-  train_data = dset.CIFAR10(root=args.data, train=True, download=True, transform=train_transform)
-  valid_data = dset.CIFAR10(root=args.data, train=False, download=True, transform=valid_transform)
-
-  train_queue = torch.utils.data.DataLoader(
-      train_data, batch_size=args.batch_size, shuffle=True, pin_memory=True, num_workers=2)
-
-  valid_queue = torch.utils.data.DataLoader(
-      valid_data, batch_size=args.batch_size, shuffle=False, pin_memory=True, num_workers=2)
-
-  scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, float(args.epochs))
-
-  best_val_acc = 0.
-  for epoch in range(args.epochs):
-    scheduler.step()
-    logging.info('epoch %d lr %e', epoch, scheduler.get_lr()[0])
-    model.drop_path_prob = args.drop_path_prob * epoch / args.epochs
-
-    train_acc, train_obj = train(train_queue, model, criterion, optimizer)
-    logging.info('train_acc %f', train_acc)
-
-    with torch.no_grad():
-      valid_acc, valid_obj = infer(valid_queue, model, criterion)
-      if valid_acc > best_val_acc:
-        best_val_acc = valid_acc
-        utils.save(model, os.path.join(args.save, 'best_weights.pt'))
-      logging.info('valid_acc %f\tbest_val_acc %f', valid_acc, best_val_acc)
-
-    utils.save(model, os.path.join(args.save, 'weights.pt'))
+	if not torch.cuda.is_available():
+		logging.info('no gpu device available')
+		sys.exit(1)
+	
+	np.random.seed(args.seed)
+	torch.cuda.set_device(args.gpu)
+	cudnn.benchmark = True
+	torch.manual_seed(args.seed)
+	cudnn.enabled=True
+	torch.cuda.manual_seed(args.seed)
+	logging.info('gpu device = %d' % args.gpu)
+	logging.info("args = %s", args)
+	
+	genotype = eval("genotypes.%s" % args.arch)
+	model = Network(args.init_channels, CIFAR_CLASSES, args.layers, args.auxiliary, genotype)
+	model = model.cuda()
+	
+	logging.info("param size = %fMB", utils.count_parameters_in_MB(model))
+	
+	criterion = nn.CrossEntropyLoss()
+	criterion = criterion.cuda()
+	optimizer = torch.optim.SGD(
+		model.parameters(),
+		args.learning_rate,
+		momentum=args.momentum,
+		weight_decay=args.weight_decay
+	)
+	
+	train_transform, valid_transform = utils._data_transforms_cifar10(args)
+	train_data = dset.CIFAR10(root=args.data, train=True, download=True, transform=train_transform)
+	valid_data = dset.CIFAR10(root=args.data, train=False, download=True, transform=valid_transform)
+	
+	train_queue = torch.utils.data.DataLoader(
+		train_data, batch_size=args.batch_size, shuffle=True, pin_memory=True, num_workers=2)
+	
+	valid_queue = torch.utils.data.DataLoader(
+		valid_data, batch_size=args.batch_size, shuffle=False, pin_memory=True, num_workers=2)
+	
+	scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, float(args.epochs))
+	
+	best_val_acc = 0.
+	for epoch in range(args.epochs):
+		scheduler.step()
+		logging.info('epoch %d lr %e', epoch, scheduler.get_lr()[0])
+		model.drop_path_prob = args.drop_path_prob * epoch / args.epochs
+		
+		train_acc, train_obj = train(train_queue, model, criterion, optimizer)
+		logging.info('train_acc %f', train_acc)
+		
+		with torch.no_grad():
+			valid_acc, valid_obj = infer(valid_queue, model, criterion)
+			if valid_acc > best_val_acc:
+				best_val_acc = valid_acc
+				utils.save(model, os.path.join(args.save, 'best_weights.pt'))
+			# logging.info('valid_acc %f\tbest_val_acc %f', valid_acc, best_val_acc)
+			logging.info('val_acc: {:.6}, best_val_acc: \033[31m{:.6}\033[0m'.format(valid_acc, best_val_acc))
+		
+		utils.save(model, os.path.join(args.save, 'weights.pt'))
 
 
 def train(train_queue, model, criterion, optimizer):
-  objs = utils.AverageMeter()
-  top1 = utils.AverageMeter()
-  top5 = utils.AverageMeter()
-  model.train()
-
-  for step, (input, target) in enumerate(train_queue):
-    input = Variable(input).cuda()
-    target = Variable(target).cuda(async=True)
-
-    optimizer.zero_grad()
-    logits, logits_aux = model(input)
-    loss = criterion(logits, target)
-    if args.auxiliary:
-      loss_aux = criterion(logits_aux, target)
-      loss += args.auxiliary_weight*loss_aux
-    loss.backward()
-    nn.utils.clip_grad_norm(model.parameters(), args.grad_clip)
-    optimizer.step()
-
-    prec1, prec5 = utils.accuracy(logits, target, topk=(1, 5))
-    n = input.size(0)
-    objs.update(loss.item(), n)
-    top1.update(prec1.item(), n)
-    top5.update(prec5.item(), n)
-
-    if step % args.report_freq == 0:
-      logging.info('train %03d %e %f %f', step, objs.avg, top1.avg, top5.avg)
-
-  return top1.avg, objs.avg
+	objs = utils.AverageMeter()
+	top1 = utils.AverageMeter()
+	top5 = utils.AverageMeter()
+	model.train()
+	
+	for step, (input, target) in enumerate(train_queue):
+		input = Variable(input).cuda()
+		target = Variable(target).cuda(async=True)
+		
+		optimizer.zero_grad()
+		logits, logits_aux = model(input)
+		loss = criterion(logits, target)
+		if args.auxiliary:
+			loss_aux = criterion(logits_aux, target)
+			loss += args.auxiliary_weight*loss_aux
+		loss.backward()
+		nn.utils.clip_grad_norm(model.parameters(), args.grad_clip)
+		optimizer.step()
+		
+		prec1, prec5 = utils.accuracy(logits, target, topk=(1, 5))
+		n = input.size(0)
+		objs.update(loss.item(), n)
+		top1.update(prec1.item(), n)
+		top5.update(prec5.item(), n)
+		
+		if step % args.report_freq == 0:
+			logging.info('train %03d %e %f %f', step, objs.avg, top1.avg, top5.avg)
+	
+	return top1.avg, objs.avg
 
 
 def infer(valid_queue, model, criterion):
-  objs = utils.AverageMeter()
-  top1 = utils.AverageMeter()
-  top5 = utils.AverageMeter()
-  model.eval()
-
-  for step, (input, target) in enumerate(valid_queue):
-    input = Variable(input).cuda()
-    target = Variable(target).cuda(async=True)
-
-    logits, _ = model(input)
-    loss = criterion(logits, target)
-
-    prec1, prec5 = utils.accuracy(logits, target, topk=(1, 5))
-    n = input.size(0)
-    objs.update(loss.item(), n)
-    top1.update(prec1.item(), n)
-    top5.update(prec5.item(), n)
-
-    if step % args.report_freq == 0:
-      logging.info('valid %03d %e %f %f', step, objs.avg, top1.avg, top5.avg)
-
-  return top1.avg, objs.avg
+	objs = utils.AverageMeter()
+	top1 = utils.AverageMeter()
+	top5 = utils.AverageMeter()
+	model.eval()
+	
+	for step, (input, target) in enumerate(valid_queue):
+		input = Variable(input).cuda()
+		target = Variable(target).cuda(async=True)
+		
+		logits, _ = model(input)
+		loss = criterion(logits, target)
+		
+		prec1, prec5 = utils.accuracy(logits, target, topk=(1, 5))
+		n = input.size(0)
+		objs.update(loss.item(), n)
+		top1.update(prec1.item(), n)
+		top5.update(prec5.item(), n)
+		
+		if step % args.report_freq == 0:
+			logging.info('valid %03d %e %f %f', step, objs.avg, top1.avg, top5.avg)
+	
+	return top1.avg, objs.avg
 
 
 if __name__ == '__main__':
-  main()
+	main()
 

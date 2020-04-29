74115001afed2d36d95a441b56da9fe85d6c7f67
5663015 <398155058@qq.com>
2020-04-29 10:36:33 +0800

    fix train.py

--- cnn/train.py
+++ cnn/train.py
@@ -19,7 +19,7 @@ from model import NetworkCIFAR as Network
 os.environ["cuda_visible_devices"] = '1'
 
 parser = argparse.ArgumentParser("cifar")
-parser.add_argument('--data', type=str, default='../data', help='location of the data corpus')
+parser.add_argument('--data', type=str, default='/home/work/dataset/cifar/', help='location of the data corpus')
 parser.add_argument('--batch_size', type=int, default=96, help='batch size')
 parser.add_argument('--learning_rate', type=float, default=0.025, help='init learning rate')
 parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
